9aef60d048188feb88544673af5289770747a7dc
TendTo <casablancaernesto@gmail.com>
2021-06-01 02:38:08 +0200

    fix(connect-elastic):  re-added geo_point

--- connect-elastic/elastic-init.sh
+++ connect-elastic/elastic-init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# until curl -X PUT "http://elasticsearch:9200/matches/" -H 'Content-Type: application/json' -d'
-#     {
-#         "mappings": {
-#             "properties": {
-#                 "location": {
-#                     "type": "geo_point"
-#                 }
-#             }
-#         }
-#     }'; do
-#   echo "curl: Elasticsearch is unavailable - retry later"
-#   sleep 10
-# done
+until curl -X PUT "http://elasticsearch:9200/matches/" -H 'Content-Type: application/json' -d'
+    {
+        "mappings": {
+            "properties": {
+                "location": {
+                    "type": "geo_point"
+                }
+            }
+        }
+    }'; do
+  echo "curl: Elasticsearch is unavailable - retry later"
+  sleep 10
+done
 
 exec /bin/connect-standalone /etc/kafka/connect-standalone.properties /etc/kafka/elasticsearch-sink.properties
